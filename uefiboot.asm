04a92a69af330901977970cefda526910eab9e0c
ThatGuyFromLol <lubiepcty59@gmail.com>
2026-03-21 19:13:21 +0100

    Update uefiboot.asm

--- uefiboot.asm
+++ uefiboot.asm
@@ -1,1 +1,14 @@
-boot
+bits 64
+section .test
+ global _start
+
+_start:
+sub rsp, 40
+mov [sys_table], rcx
+
+mov rdx, [rcx + 64] ;wskaźnik conout
+mov rcx, rdx :komenda print
+mov rcx, rdx
+lea rdx, [rel hello_str]
+call [qword[sys_table]+64 
+
