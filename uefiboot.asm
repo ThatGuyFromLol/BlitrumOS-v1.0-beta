33f1bc40a1ae9a340e793ebfe6ac1361d59c60bd
ThatGuyFromLol <lubiepcty59@gmail.com>
2026-06-03 12:48:56 +0200

    Update uefiboot.asm

--- uefiboot.asm
+++ uefiboot.asm
@@ -7,7 +7,7 @@ sub rsp, 40
 mov [sys_table], rcx
 
 mov rdx, [rcx + 64] ;wskaźnik conout
-mov rcx, rdx :komenda print
+mov rcx, rdx ;komenda print
 mov rcx, rdx
 lea rdx, [rel hello_str]
 call [qword[sys_table]+64 
