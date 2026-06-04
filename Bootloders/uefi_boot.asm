f22b178133f822aa1689eba9208b3594f456851b
ThatGuyFromLol <lubiepcty59@gmail.com>
2026-06-04 03:22:52 +0200

    Update uefi_boot.asm

--- Bootloders/uefi_boot.asm
+++ Bootloders/uefi_boot.asm
@@ -1,14 +1,28 @@
 bits 64
-section .test
+section .text
  global _start
 
 _start:
 sub rsp, 40
-mov [sys_table], rcx
+mov [image_handle], rcx
+mov [sys_table], rdx
 
-mov rdx, [rcx + 64] ;wskaźnik conout
-mov rcx, rdx ;komenda print
-mov rcx, rdx
+mov rbx, [rdx + 64] ;wskaźnik conout
+mov rcx, rbx        ; komenda print
 lea rdx, [rel hello_str]
-call [qword[sys_table]+64]
+call qword [rbx + 8]
 
+;tutaj load Kernel
+
+mov rax ,0x00100000
+jmp rax
+
+hang:
+hot
+jmp hang
+
+section .data
+
+image_handle dq 0
+
+sys_table dq 0
